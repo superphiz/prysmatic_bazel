80ab9c4da74b5b6b23c39aabba0ff3dfcfad381e
superphiz <superphiz@protonmail.com>
2020-01-18 15:48:06 +0000

    making ethstats look fancier for others

--- eth2stats.sh
+++ eth2stats.sh
@@ -1,19 +1,13 @@
-docker stop eth2stats
+#!/bin/sh
 
+#This will stop any currently running docker image for ethstats
+docker stop eth2stats
 docker rm eth2stats
 
+#Update the image
 docker pull alethio/eth2stats-client:latest
 
-#docker run -d --name eth2stats-client --restart always --network="host" \
-#    alethio/eth2stats-client:latest \
-#    run --v \
-#    --eth2stats.node-name="superphiz" \
-#    --eth2stats.addr="grpc.sapphire.eth2stats.net:443" --eth2stats.tls=true \
-#    --beacon.type="prysm" --beacon.addr="localhost:4000"
-
-#       --beacon_metrics.addr="http://sapphire-prysm:8080/metrics" \
-
-
+#start/restart the image
 docker run -d --name eth2stats --restart always --network="host" \
       -v ~/eth2stats/data:/data \
       alethio/eth2stats-client:latest \
