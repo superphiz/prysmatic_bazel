c1dbe5ad2e9528ef17c8399ac0a9b727c24c2c81
superphiz <51464737+superphiz@users.noreply.github.com>
2020-01-23 11:54:03 -0600

    Update create_wallet.sh

--- create_wallet.sh
+++ create_wallet.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd $HOME/prysm && $HOME/bin/bazel run //validator:validator accounts create -- --keystore-path=/usr/local/prysm --password 12345678 2>&1 | tee $HOME/deposit_data_info_for_prylabs_dot_net.txt
+cd $HOME/prysm && $HOME/bin/bazel run //validator:validator accounts create -- --keystore-path=$HOME/prysm --password 12345678 2>&1 | tee $HOME/deposit_data_info_for_prylabs_dot_net.txt
