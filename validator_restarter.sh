8128cf045b02c4f98323533fef9e0b0dae025e82
superphiz <51464737+superphiz@users.noreply.github.com>
2020-01-26 14:20:32 -0600

    Trying the new keystore method
    https://github.com/prysmaticlabs/prysm/blob/master/validator/keymanager/direct_keystore.go

--- validator_restarter.sh
+++ validator_restarter.sh
@@ -4,5 +4,6 @@ while true
 do
 	cd $HOME/prysm && /usr/bin/git pull
 	cd $HOME/prysm && $HOME/bin/bazel build //validator:validator
-	cd $HOME/prysm && unbuffer $HOME/bin/bazel run validator -- --datadir=$HOME/prysm/keystore --keymanager=keystore --keymanageropts='12345678' --password="12345678" 2>&1 | tee $HOME/prysm/validator.log
+	cd $HOME/prysm && unbuffer $HOME/bin/bazel run validator -- --datadir=$HOME/prysm/keystore --keymanager=keystore --keymanageropts= 
+	 "path":   "$HOME/prysm/keystore", "passphrase": "12345678"}, --password="12345678" 2>&1 | tee $HOME/prysm/validator.log
 done
